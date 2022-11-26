5ccc8c5325051c64138a1777d7f9d06db5fcf4b9
Peter Hosey <git@boredzo.org>
2022-11-26 14:48:26 -0800

    Initial Commit

---
+++ impluse-hfs/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  impluse-hfs
+//
+//  Created by Peter Hosey on 2022-11-26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
