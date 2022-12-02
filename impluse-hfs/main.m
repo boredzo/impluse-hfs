a4ebdc1766f98540b63564d222cde20eae809c28
Peter Hosey <git@boredzo.org>
2022-12-02 11:45:02 -0800

    Refactor this to use an object, which will be the place to add subcommands.

--- impluse-hfs/main.m
+++ impluse-hfs/main.m
@@ -10,33 +10,72 @@
 
 #import "ImpHFSToHFSPlusConverter.h"
 
+@interface Impluse : NSObject
+
+@property(copy) NSString *argv0;
+@property int status;
+
+- (void) printUsageToFile:(FILE *_Nonnull const)outputFile;
+
+- (void) help:(NSEnumerator <NSString *> *_Nonnull const)argsEnum;
+- (void) convert:(NSEnumerator <NSString *> *_Nonnull const)argsEnum;
+- (void) extract:(NSEnumerator <NSString *> *_Nonnull const)argsEnum;
+
+@end
 static void usage(char const *_Nullable const argv0) {
-	printf("usage: %s hfs-device hfsplus-device\n", argv0 ?: "impluse");
-	printf("The two paths must not be the same. The contents of hfs-device will be copied to hfsplus-device. This may take some time.\n");
 }
 
 int main(int argc, const char * argv[]) {
+	int status = EXIT_SUCCESS;
 	@autoreleasepool {
 		NSEnumerator <NSString *> *_Nonnull const argsEnum = [[NSProcessInfo processInfo].arguments objectEnumerator];
-		[argsEnum nextObject];
-		NSString *_Nonnull const srcDevPath = [argsEnum nextObject];
-		NSString *_Nonnull const dstDevPath = [argsEnum nextObject];
-		if (! (srcDevPath != nil && dstDevPath != nil)) {
-			usage(argv[0]);
-			return EX_USAGE;
-		}
-		ImpHFSToHFSPlusConverter *_Nonnull const converter = [ImpHFSToHFSPlusConverter new];
-		converter.sourceDevice = [NSURL fileURLWithPath:srcDevPath isDirectory:false];
-		converter.destinationDevice = [NSURL fileURLWithPath:dstDevPath isDirectory:false];
-		converter.conversionProgressUpdateBlock = ^(float progress, NSString * _Nonnull operationDescription) {
-			ImpPrintf(@"%u%%: %@", (unsigned)round(100.0 * progress), operationDescription);
-		};
-		NSError *_Nullable error = nil;
-		bool const converted = [converter performConversionOrReturnError:&error];
-		if (! converted) {
-			NSLog(@"Failed: %@", error.localizedDescription);
-			return EXIT_FAILURE;
-		}
+
+		Impluse *_Nonnull const impluse = [Impluse new];
+		impluse.argv0 = [argsEnum nextObject];
+
+		NSString *_Nonnull const subcommand = [argsEnum nextObject];
+		[impluse performSelector:NSSelectorFromString([subcommand stringByAppendingString:@":"]) withObject:argsEnum];
+
+		status = impluse.status;
 	}
-	return EXIT_SUCCESS;
+	return status;
 }
+
+@implementation Impluse
+
+- (void) printUsageToFile:(FILE *_Nonnull const)outputFile {
+	fprintf(outputFile, "usage: %s convert hfs-device hfsplus-device\n", self.argv0.UTF8String ?: "impluse");
+	fprintf(outputFile, "The two paths must not be the same. The contents of hfs-device will be copied to hfsplus-device. This may take some time.\n");
+}
+
+- (void) help:(NSEnumerator <NSString *> *_Nonnull const)argsEnum {
+	[self printUsageToFile:stdout];
+}
+- (void) convert:(NSEnumerator <NSString *> *_Nonnull const)argsEnum {
+	NSString *_Nonnull const srcDevPath = [argsEnum nextObject];
+	NSString *_Nonnull const dstDevPath = [argsEnum nextObject];
+	if (! (srcDevPath != nil && dstDevPath != nil)) {
+		[self printUsageToFile:stderr];
+		self.status = EX_USAGE;
+		return;
+	}
+
+	ImpHFSToHFSPlusConverter *_Nonnull const converter = [ImpHFSToHFSPlusConverter new];
+	converter.sourceDevice = [NSURL fileURLWithPath:srcDevPath isDirectory:false];
+	converter.destinationDevice = [NSURL fileURLWithPath:dstDevPath isDirectory:false];
+	converter.conversionProgressUpdateBlock = ^(float progress, NSString * _Nonnull operationDescription) {
+		ImpPrintf(@"%u%%: %@", (unsigned)round(100.0 * progress), operationDescription);
+	};
+	NSError *_Nullable error = nil;
+	bool const converted = [converter performConversionOrReturnError:&error];
+	if (! converted) {
+		NSLog(@"Failed: %@", error.localizedDescription);
+		self.status = EXIT_FAILURE;
+	}
+
+}
+- (void) extract:(NSEnumerator <NSString *> *_Nonnull const)argsEnum {
+	//TODO: Write an ImpHFSExtractor that implements at least this feature, similar to how we have ImpHFSToHFSPlusConverter for convert:.
+}
+
+@end
