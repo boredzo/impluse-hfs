6ed5bd4e8ed2941468794218340746a2d3e1c039
Peter Hosey <git@boredzo.org>
2022-12-26 00:00:23 -0800

    Oh yeah, we need to close forks after we open them. (Fixes failing to rehydrate entire volumes with too many files to have all of them open at once.)

--- impluse-hfs/ImpDehydratedItem.m
+++ impluse-hfs/ImpDehydratedItem.m
@@ -310,6 +310,7 @@ static NSTimeInterval hfsEpochTISRD = -3061152000.0; //1904-01-01T00:00:00Z time
 		}
 		return allWritesSucceeded;
 	}];
+	FSCloseFork(dataForkRefnum);
 	if (writeError != nil) {
 		if (outError != NULL) {
 			*outError = writeError;
@@ -337,6 +338,7 @@ static NSTimeInterval hfsEpochTISRD = -3061152000.0; //1904-01-01T00:00:00Z time
 		}
 		return allWritesSucceeded;
 	}];
+	FSCloseFork(rsrcForkRefnum);
 	if (! allWritesSucceeded) {
 		if (outError != NULL) {
 			*outError = writeError;
