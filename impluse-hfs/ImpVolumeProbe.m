8aa4618eff3d2c6a14b1d50ba21f0fcc97d923a2
Peter Hosey <git@boredzo.org>
2023-02-28 17:00:11 -0800

    More #7: Implement searching the partition map for HFS volumes. Could use some more diagnostics in case we come up empty-handed (particularly in the case of finding a partition map but no HFS volumes, or what should be an HFS volume but it's not HFS or HFS+), but for now, this works.

--- impluse-hfs/ImpVolumeProbe.m
+++ impluse-hfs/ImpVolumeProbe.m
@@ -26,6 +26,54 @@
 
 @end
 
+//IM:D calls this structure “Block0”. All fields are big-endian.
+//We don't really care about trying to parse the driver descriptor block, other than checking the signature. If we find the driver descriptor block, we can expect it to be followed immediately by the partition map.
+//(Note that absence of a DDB does not imply absence of an APM. There can be a PM block with no preceding ER block.)
+enum {
+	APMDriverDescriptorBlockSignature = 0x4552, //'ER'
+	APMDriverDescriptorBlockSignatureAlternate = 0x0000, //Sometimes there's no Driver Descriptor Record but block 0 is still followed by one or more partition map entries.
+
+	//IM:D: “This field should contain the value of the pMapSIG constant ($504D). An earlier but still supported version uses the value $5453.”
+	APMPartitionMapEntrySignature = 0x504D, //'PM'
+	APMPartitionMapEntrySignatureOldStyle = 0x5453, //'ST'
+};
+struct APMDriverDescriptorBlock {
+	u_int16_t signature; //'ER'
+	u_int16_t bytesPerBlock;
+	u_int32_t numBlocks;
+	u_int16_t devType; //reserved
+	u_int16_t devID; //reserved
+	u_int32_t data; //reserved
+	u_int16_t numDrivers;
+	struct APMDriverRecord {
+		u_int32_t driverStartBlock;
+		u_int16_t driverBlockCount;
+		u_int16_t operatingSystemType; //Mac OS == 1
+	} driverRecords[(0x200 - (2+2+4+2+2+4+2)) / 8];
+	u_int32_t pad; //reserved
+};
+struct APMPartition {
+	u_int16_t signature; //'PM'
+	u_int16_t signaturePad;
+	u_int32_t numMapBlocks;
+	u_int32_t thisPartitionStartBlock;
+	u_int32_t thisPartitionBlockCount;
+	char partitionName[32]; //Not as useful as you might think. Often contradicted by the volume header, partly because naming it something starting with 'Maci' (like "Macintosh HD") triggers the boot code checksum verification.
+	char partitionType[32]; //Much more useful. "Apple_HFS" is the type we're looking for.
+	u_int32_t dataStartBlock; //Logical block—relative to thisPartitionStartBlock.
+	u_int32_t dataBlockCount;
+	u_int32_t status; //“currently used only by the A/UX operating system”
+	u_int32_t bootStartBlock;
+	u_int32_t bootCodeLengthInBytes;
+	u_int32_t bootLoadAddress0;
+	u_int32_t bootLoadAddress1; //reserved
+	u_int32_t bootEntryPoint0;
+	u_int32_t bootEntryPoint1; //reserved
+	u_int32_t bootCodeChecksum;
+	Str15 processorType;
+	u_int16_t pad[188];
+};
+
 @implementation ImpVolumeProbe
 {
 	int _readFD;
@@ -112,7 +160,81 @@
 #pragma mark -
 
 - (bool) scanApplePartitionMap {
-	return false;
+	enum {
+		sbSIGWord = 0x4552, //'ER'
+		pMapSIG = 0x504D, //'PM'
+		pMapSIGOldStyle = 0x5453, //'TS'
+	};
+
+	NSUInteger firstPMBlock = 0;
+	NSData *_Nonnull const firstBlock = [self readOneISOStandardBlockAtIndex:0];
+	struct APMDriverDescriptorBlock const *_Nonnull const maybeDDB = (void const *)(firstBlock.bytes);
+	switch (L(maybeDDB->signature)) {
+		case APMDriverDescriptorBlockSignature:
+			//It's a DDB. We should definitely have an APM, just after the DDB.
+		case APMDriverDescriptorBlockSignatureAlternate:
+			//If the space where the DDB should be is empty, the partition map might start in the second block. (It certainly isn't in the *first* block, anyway.)
+			firstPMBlock = 1;
+			break;
+
+		default:
+			//Whatever this is, it doesn't look like a DDB. Reject it.
+			return false;
+	}
+
+	bool foundPartitionMap = false;
+
+	NSUInteger partitionIndex = 0;
+	NSUInteger numPartitions = 0;
+	do {
+		NSData *_Nullable partitionMapEntryData = [self readOneISOStandardBlockAtIndex:firstPMBlock + partitionIndex];
+		if (partitionMapEntryData != nil) {
+			struct APMPartition const *_Nonnull const partition = partitionMapEntryData.bytes;
+			if (numPartitions == 0) {
+				switch (L(partition->signature)) {
+					case APMPartitionMapEntrySignature:
+					case APMPartitionMapEntrySignatureOldStyle:
+						foundPartitionMap = true;
+						break;
+					default:
+						//Signature mismatch—this is not an APM entry. And since it's supposed to be first APM entry, that means there's no APM.
+						foundPartitionMap = false;
+						break;
+				}
+				if (! foundPartitionMap) {
+					break;
+				}
+
+				numPartitions = L(partition->numMapBlocks);
+			}
+
+			if (strcmp(partition->partitionType, "Apple_HFS") == 0) {
+				u_int32_t const hfsBootBlocksStartBlockIndex = L(partition->thisPartitionStartBlock);
+				u_int32_t const hfsVolumeHeaderBlockIndex = hfsBootBlocksStartBlockIndex + 2;
+				NSData *_Nonnull const thirdVolumeBlockData = [self readOneISOStandardBlockAtIndex:hfsVolumeHeaderBlockIndex];
+
+				Class _Nullable identifiedClass = Nil;
+
+				struct HFSMasterDirectoryBlock const *_Nonnull const mdbPtr = (struct HFSMasterDirectoryBlock const *_Nonnull)thirdVolumeBlockData.bytes;
+				if (L(mdbPtr->drSigWord) == kHFSSigWord) {
+					identifiedClass = [ImpHFSVolume class];
+				} else {
+					struct HFSPlusVolumeHeader const *_Nonnull const vhPtr = (struct HFSPlusVolumeHeader const *_Nonnull)thirdVolumeBlockData.bytes;
+					if (L(vhPtr->signature) == kHFSPlusSigWord) {
+						identifiedClass = [ImpHFSPlusVolume class];
+					} else {
+						//Signature isn't HFS or HFS+? This may be mapped as an Apple_HFS partition, but it's not a usable HFS or HFS+ volume. Skip it.
+						continue;
+					}
+				}
+
+				u_int32_t const hfsVolumeBlockCount = L(partition->thisPartitionBlockCount);
+				[self foundVolumeStartingAtBlock:hfsBootBlocksStartBlockIndex blockCount:hfsVolumeBlockCount class:identifiedClass];
+			}
+		}
+	} while (partitionIndex++ < numPartitions);
+
+	return foundPartitionMap;
 }
 - (bool) scanBareVolumeStartingAtBlock:(u_int64_t const)startBlock blockCount:(u_int64_t const)numBlocks  {
 	NSData *_Nonnull const thirdBlock = [self readOneISOStandardBlockAtIndex:startBlock + 2];
